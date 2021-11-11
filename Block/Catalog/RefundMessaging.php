e2ef37b18d52c4a94c6370c473477249794f0aa6
ilan <ilan@refundid.com>
2021-11-11 07:10:51 +0000

    feat: add refundid magic to magento - default

---
+++ Block/Catalog/RefundMessaging.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Magento 2 extensions for Refundid
+ *
+ * @author Refundid
+ */
+
+namespace Refundid\CreditMemo\Block\Catalog;
+
+use Magento\Framework\View\Element\Template;
+
+
+class RefundMessaging extends Template {
+    public $isActive = False;
+}
